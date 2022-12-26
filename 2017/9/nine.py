e48ff72ff7d9617adc79a014407f8a755399cd5f
carsonmagnuson <carsonmagnuson@gmail.com>
2022-12-25 21:16:55 -0500

    cleaning code

--- 2017/9/nine.py
+++ 2017/9/nine.py
@@ -1,23 +1,15 @@
 i_n = open('2017/9/input.txt').read()
 
 def both_parts(line):
-    score = 0
-    current_value = 1
-    trash = False
-    index = 0
-    non_trash_count = 0
+    score, index, non_trash_count, current_value, trash = 0, 0, 0, 1, False
     while index < len(line) - 1:
         if not trash:
-            if line[index] == '{':
-                score += current_value
-                current_value += 1
+            if line[index] == '{': score, current_value = score + current_value, current_value + 1
             elif line[index] == '}': current_value -= 1
             elif line[index] == '<': trash = True
             elif line[index] == '!': index += 1
         elif line[index] == '>': trash = False
-        elif line[index] == '!':
-            index += 2
-            continue
+        elif line[index] == '!': index += 1
         else: non_trash_count += 1
         index += 1
     return (score, non_trash_count)
