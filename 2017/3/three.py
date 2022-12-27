4e222679d27fc7ae5c566be7c340b40a52ea574b
carsonmagnuson <carsonmagnuson@gmail.com>
2022-12-27 08:10:15 +0100

    progress on day 3 pt 2 2017

--- 2017/3/three.py
+++ 2017/3/three.py
@@ -20,15 +20,52 @@ def part_one(number):
     # return steps
 
 def part_two(number):
-    transforms = [(-1, -1), (1, 1), (-1, 1), (1, -1), (1, 0), (-1, 0), (0, 1), (0, -1)]
+    transforms = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
     matrix = [[0 for _ in range(math.ceil(math.sqrt(number)) + 1)] for _ in range(math.ceil(math.sqrt(number)) + 1)]
     pos = (len(matrix)//2, (len(matrix)//2))
     matrix[pos[1]][pos[0]] = 1
+    layer = 1
     
 
-    while matrix[pos[1]][pos[0]] <= number: ## I have to traverse the array in a circular pattern. I think I can do it based on whats around the number
-        for transform in transforms:
+    while True: ## I have to traverse the array in a circular pattern. I think I can do it based on whats around the number. We can push the position a w a y from other numbers we find around it, that way we don't have bunch of conditionals. Okay actually no. I think there is easier way.
+        adjaciency_list = []
+        layer += 2
+
+        perimeter = (layer - 1) * 4
+        
+        # another idea for calculating out the length of the sides is what Alex told me, the odd-sqrt - 1. Using this we can just count for each side and the increase count per layer.
+        for perimeter_count in range(0, perimeter):
+            if perimeter_count == 0:
+                pos[0] += 1
+            elif perimeter_count < perimeter//4:
+                pos[1] += 1
+            elif perimeter_count < perimeter//2:
+                pos[0] -= 1
+            elif perimeter_count < perimeter//4 + perimeter//2:
+                pos[1] -= 1
+            else:
+                pos[0] += 1
+
+            for x_transf, y_transf in transforms:
+                adjaciency_list.append(matrix[pos[1] + y_transf][pos[0] + x_transf])
+            next_val = sum(el for el in adjaciency_list)
+            if next_val > number:
+                return matrix[pos[1]][pos[0]]
+
             
+
+            
+           
+           
+            
+
+
+
+
+
+
+
+        
 
 
 
