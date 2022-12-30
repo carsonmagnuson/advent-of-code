82961c130cf19f7fe2f237cc3952a6b1b553713b
carsonmagnuson <carsonmagnuson@gmail.com>
2022-12-30 09:42:23 +0100

    working on pt 1 day 16 2022

--- 2022/16/sixteen.py
+++ 2022/16/sixteen.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from typing import List
 
 
-i_n = list(list(int(i) if ord(i[0]) >= 48 and ord(i[0]) <= 57 else i for i in findall(r"-?\d+|[A-Z]{2}", l)) for l in open('input.txt').read().splitlines())
+i_n = list(list(int(i) if ord(i[0]) >= 48 and ord(i[0]) <= 57 else i for i in findall(r"-?\d+|[A-Z]{2}", l)) for l in open('2022/16/input.txt').read().splitlines())
 
 
 
@@ -25,17 +25,31 @@ class Valve:
     neighbors: List[Neighbor] 
     flow_rate: int
      
-
-def get_neighbors(input, valve, valves, count, visited):
-    visited.add(valve[0])
-    weighted_neighbors = []
-    for neighbor_index in range(2, len(valve)):
-        if valve[neighbor_index] not in visited:
-            if valves.get(valve[neighbor_index]) != None:
-                weighted_neighbors.append(Neighbor(count, valve[neighbor_index]))
+def get_valid_neighbors(visited, current_valve, all_valves, valid_valves, count):
+    visited.add(current_valve)
+    valid_neighbors = []
+    for neighbor in all_valves[current_valve]:
+        if neighbor not in visited:
+            if valid_valves.get(neighbor) != None:
+                valid_neighbors.append(Neighbor(neighbor, count))
             else:
-                weighted_neighbors.extend(get_neighbors(input, valve[neighbor_index], valves, count + 1, visited)) 
-    return weighted_neighbors
+                valid_neighbors.extend(get_valid_neighbors(visited, neighbor, all_valves, valid_valves, count + 1))
+    return valid_neighbors
+
+
+
+
+
+# def get_neighbors(input, valve, valves, count, visited):
+#     visited.add(valve[0])
+#     weighted_neighbors = []
+#     for neighbor_index in range(2, len(valve)):
+#         if valve[neighbor_index] not in visited:
+#             if valves.get(valve[neighbor_index]) != None:
+#                 weighted_neighbors.append(Neighbor(count, valve[neighbor_index]))
+#             else:
+#                 weighted_neighbors.extend(get_neighbors(input, valve[neighbor_index], valves, count + 1, visited)) 
+#     return weighted_neighbors
         
 
 def convert_to_map(input):
@@ -45,22 +59,24 @@ def convert_to_map(input):
     return connections
 
 
-def convert_to_weighted(input):
-
-    valves = {}
+def convert_to_weighted(input, all_valves):
+    valid_valves = {}
     for valve in input:
         if valve[1] > 0 or valve[0] == "AA":
             newValve = Valve(valve[0], [], valve[1])
-            valves[newValve.name] = newValve
-    
-    print(get_neighbors(i_n, i_n[0], valves, 1, set()))
-
-    # for valve in valves.keys():
-    #     get_neighbors()
-    return valves
+            valid_valves[newValve.name] = newValve
+    for valve in valid_valves.keys():
+        valid_valves[valve].neighbors = get_valid_neighbors(set(), valve, all_valves, valid_valves, 1)
+    return valid_valves
 
 
-print(convert_to_map(i_n))
+un_weighted = convert_to_map(i_n)
+valid_valves = convert_to_weighted(i_n, un_weighted)
+for valve in valid_valves.keys():
+    print(valid_valves[valve])
+
+# print(get_valid_neighbors(set(), 'BB', un_weighted, valid_valves, 1))
+
 # valves = convert_to_weighted(i_n)
 # valves['BB'].neighbors += 
 # print(valves)
