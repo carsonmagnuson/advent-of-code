be669a860bdf4cde39682010573a13f924cc510e
carsonmagnuson <carsonmagnuson@gmail.com>
2023-02-19 19:36:10 +0100

    solve day 20 2017

---
+++ 2017/20/twenty.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from copy import deepcopy
+from re import findall
+
+@dataclass
+class Vertex:
+    x: int
+    y: int
+    z: int
+
+@dataclass
+class Particle:
+    pos: Vertex
+    veloc: Vertex
+    accel: Vertex
+    name: int
+
+input = list(findall('-?\d+', i) for i in open('input.txt').read().splitlines())
+particles = list(Particle(Vertex(*map(int, [x for x in input[i][0:3]])), Vertex(*map(int, [x for x in input[i][3:6]])), Vertex(*map(int, [x for x in input[i][6:9]])), i) for i in range(len(input)))
+def brute(particles, iterations):
+    for i in range(0, iterations):
+        for par in particles:
+            par.veloc = Vertex(par.veloc.x + par.accel.x, par.veloc.y + par.accel.y, par.veloc.z + par.accel.z)
+            par.pos = Vertex(par.pos.x + par.veloc.x, par.pos.y + par.veloc.y, par.pos.z + par.veloc.z)
+    return min(particles, key=lambda i: abs(i.pos.x) + abs(i.pos.y) + abs(i.pos.z)).name
+
+def collidascope(particles, iterations):
+    for i in range(0, iterations):
+        collision = {}
+        new = []
+        for par in particles:
+            par.veloc = Vertex(par.veloc.x + par.accel.x, par.veloc.y + par.accel.y, par.veloc.z + par.accel.z)
+            par.pos = Vertex(par.pos.x + par.veloc.x, par.pos.y + par.veloc.y, par.pos.z + par.veloc.z)
+            collision[str(par.pos)] = collision.get(str(par.pos), 0) + 1
+        for index in range(len(particles)):
+            if collision.get(str(particles[index].pos)) < 2:
+                new.append(particles[index])
+        particles = new
+    return len(particles)
+
+print(brute(deepcopy(particles), 1000))
+print(collidascope(particles, 1000))
