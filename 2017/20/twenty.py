65c39362625ad204033a15406b9f5aba7d4ca616
carsonmagnuson <carsonmagnuson@gmail.com>
2023-02-19 19:46:08 +0100

    tidy up day 20 2017 code

--- 2017/20/twenty.py
+++ 2017/20/twenty.py
@@ -17,26 +17,28 @@ class Particle:
 
 input = list(findall('-?\d+', i) for i in open('input.txt').read().splitlines())
 particles = list(Particle(Vertex(*map(int, [x for x in input[i][0:3]])), Vertex(*map(int, [x for x in input[i][3:6]])), Vertex(*map(int, [x for x in input[i][6:9]])), i) for i in range(len(input)))
-def brute(particles, iterations):
+
+def traverse(collision, particles, iterations):
+    if collision: collisions = {}
     for i in range(0, iterations):
         for par in particles:
             par.veloc = Vertex(par.veloc.x + par.accel.x, par.veloc.y + par.accel.y, par.veloc.z + par.accel.z)
             par.pos = Vertex(par.pos.x + par.veloc.x, par.pos.y + par.veloc.y, par.pos.z + par.veloc.z)
-    return min(particles, key=lambda i: abs(i.pos.x) + abs(i.pos.y) + abs(i.pos.z)).name
+            if collision: collisions[str(par.pos)] = collisions.get(str(par.pos), 0) + 1
+        if collision:
+            new = []
+            for index in range(len(particles)):
+                if collisions.get(str(particles[index].pos)) < 2:
+                    new.append(particles[index])
+            particles = new
+    return particles
+
+
+def brute(particles, iterations):
+    return min(traverse(False, deepcopy(particles), iterations), key=lambda i: abs(i.pos.x) + abs(i.pos.y) + abs(i.pos.z)).name
 
 def collidascope(particles, iterations):
-    for i in range(0, iterations):
-        collision = {}
-        new = []
-        for par in particles:
-            par.veloc = Vertex(par.veloc.x + par.accel.x, par.veloc.y + par.accel.y, par.veloc.z + par.accel.z)
-            par.pos = Vertex(par.pos.x + par.veloc.x, par.pos.y + par.veloc.y, par.pos.z + par.veloc.z)
-            collision[str(par.pos)] = collision.get(str(par.pos), 0) + 1
-        for index in range(len(particles)):
-            if collision.get(str(particles[index].pos)) < 2:
-                new.append(particles[index])
-        particles = new
-    return len(particles)
+    return len(traverse(True, deepcopy(particles), iterations))
 
-print(brute(deepcopy(particles), 1000))
+print(brute(particles, 1000))
 print(collidascope(particles, 1000))
