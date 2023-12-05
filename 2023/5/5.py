b0f48ebf84168b2e2d2540cf3a26e067e05e6b75
Carson Magnuson <carsonmagnuson@gmail.com>
2023-12-05 12:16:17 -0500

    solve 2023 day 5 pt 2

--- 2023/5/5.py
+++ 2023/5/5.py
@@ -1,8 +1,9 @@
-i = open('test.txt', 'r').read().split('\n\n')
+i = open('input.txt', 'r').read().split('\n\n')
 
 i = [line.strip() for line in i]
 
 seeds = list(map(int, i[0][i[0].find(':')+1:].split()))
+seedsv2 = list((seeds[a], seeds[a+1]) for a in range(0, len(seeds), 2)) 
 
 def wormhole(grav_map, seed):
     grav_map = grav_map.split()[2:]
@@ -14,6 +15,7 @@ def wormhole(grav_map, seed):
 
         
 seed_bank = []
+dominant = float('inf')
 
 for seed in seeds:
     gmo = seed
@@ -21,4 +23,19 @@ for seed in seeds:
         gmo = wormhole(grav_map, gmo)
     seed_bank.append(gmo)
 
-print(min(seed_bank))
+print('Spinning up seed contest part two...')
+for start, delta in seedsv2:
+    for seed in range(start, start+delta-1):
+        if seed%100000000 == 0:
+            print(f'seeding number {seed}')
+        for grav_map in i[1:]:
+            seed = wormhole(grav_map, seed)
+        dominant = min(seed, dominant)
+
+#test = 82
+#for grav_map in i[1:]:
+    #test = wormhole(grav_map, test)
+#print(test)
+
+print(dominant)
+# print(min(seed_bank))
