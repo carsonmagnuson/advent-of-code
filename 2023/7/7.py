109c50844d811269a372cc34389cc62f8933f0b8
Carson Magnuson <carsonmagnuson@gmail.com>
2023-12-07 02:01:10 -0500

    make cleanererer

--- 2023/7/7.py
+++ 2023/7/7.py
@@ -9,20 +9,18 @@ for line in i:
     hands.append((hand, int(bid)))
 
 def ranker(card):
-    freq = {}
-    heavy = 'J'
+    freq, heavy = {}, 'J'
     for char in card:
         freq[char] = freq.get(char, 0) + 1
-        heavy = max([char, heavy], key=lambda x: weight[x])
+
+    heavy = max(freq.keys(), key=lambda x: weight[x])
 
     if freq.get('J') and freq['J'] != 5:
         jokes = freq['J']
         del freq['J']
         freq[heavy if len(freq.keys()) == 5 else max(freq.keys(), key=lambda x: freq[x])] += jokes
 
-
-    big = max(freq.values())
-    variance = len(freq.keys())
+    big, variance = max(freq.values()), len(freq.keys()) 
 
     if big >= 4: return big + 2
     if big == 3: return 5 if variance == 2 else 4
@@ -31,8 +29,7 @@ def ranker(card):
 
 def compare(A, B):
     diff = ranker(A[0]) - ranker(B[0])
-    if diff != 0: return (diff) / abs(diff)
-    return weigh_deez(A[0], B[0])
+    return (diff) / abs(diff) if diff != 0 else weigh_deez(A[0], B[0])
 
 def weigh_deez(a, b):
     for index in range(5):
