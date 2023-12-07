83821ced1c31358d55245a0c4476524e591f9594
Carson Magnuson <carsonmagnuson@gmail.com>
2023-12-07 01:25:59 -0500

    solve 2023 day 7 pt 2

--- 2023/7/7.py
+++ 2023/7/7.py
@@ -2,6 +2,7 @@ from functools import cmp_to_key
 i = open('input.txt', 'r').read().splitlines()
 
 hands = []
+weight = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 1, 'Q': 12, 'K': 13, 'A': 14}
 
 for line in i:
     hand, bid = line.split()
@@ -10,12 +11,26 @@ for line in i:
 def ranker(card):
     freq = {}
     big = 0
-    smoll = 10
+    heavy = 'J'
     for char in card:
         freq[char] = freq.get(char, 0) + 1
-        smoll = min(smoll, freq[char])
-        big = max(big, freq[char])
+        heavy = max([char, heavy], key=lambda x: weight[x])
+
+    if freq.get('J') and freq['J'] != 5:
+        jokes = freq['J']
+        del freq['J']
+        if len(freq.keys()) == 5: 
+            freq[heavy] += jokes
+        else:
+            freq[max(freq.keys(), key=lambda x: freq[x])] += jokes
+
+
+    big = max(freq.values())
     if big >= 4:
+        if big > 5:
+            print('oops')
+            print(big)
+            print(freq)
         return big + 2
     if big == 3:
         return 5 if len(freq.keys()) == 2 else 4
@@ -32,9 +47,10 @@ def compare(A, B):
         return 1
     if a_rank < b_rank:
         return -1
-    
-    weight = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 
+    return weigh_deez(a, b)
+
+def weigh_deez(a, b):
     for index in range(5):
         a_weight = weight[a[index]]
         b_weight = weight[b[index]]
