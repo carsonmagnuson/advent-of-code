a37f4d8dd908e944ef51b9928fbdfad66eb74e30
carsonmagnuson <carsonmagnuson@gmail.com>
2023-12-11 10:30:41 -0500

    create brute force 2023 day 8

--- 2023/8/8.py
+++ 2023/8/8.py
@@ -1,21 +1,36 @@
 import re
-i = open('test3.txt').read().splitlines()
+i = open('input.txt').read().splitlines()
 
 nodes = {}
+starts = []
 directions = i[0].strip()
 print(directions)
 
-for line in i[2:]:
-    capitals = ''.join(re.findall(r'[A-Z]', line))
+for index, line in enumerate(i[2:]):
+    capitals = ''.join(re.findall(r'[A-Z0-9]', line))
+#r'[A-Z]'
     nodes[capitals[0:3]] = (capitals[3:6], capitals[6:9])
+    print(capitals)
+    if capitals[2] == 'A':
+        starts.append(capitals[0:3])
 
-place = 'AAA'
-moves = 1
+#place = 'AAA'
+#moves = 1
 go = {'L':0, 'R':1}
-while place != 'ZZZ':
+#while place != 'ZZZ':
+#    curr = directions[moves%len(directions)-1]
+#    place = nodes[place][go[curr]]
+#    moves += 1
+
+
+moves = 1
+while set(i[2] for i in starts) != set(['Z']):
+
     curr = directions[moves%len(directions)-1]
-    place = nodes[place][go[curr]]
+    for index, start in enumerate(starts):
+        starts[index] = nodes[start][go[curr]]
     moves += 1
+
 
 print(moves-1)
 
