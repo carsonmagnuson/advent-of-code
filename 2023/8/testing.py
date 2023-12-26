556419d38d41b52dbdc4da0d7070cc98ccb6e0c9
Carson Magnuson <carsonmagnuson@gmail.com>
2023-12-26 01:23:46 -0500

    solve day 8 part 2 2023 through sheer will in testing py

--- 2023/8/testing.py
+++ 2023/8/testing.py
@@ -1,68 +1,57 @@
 import re
 import math
-i = open('input2.txt').read().splitlines()
+i = open('input.txt').read().splitlines()
 
 nodes = {}
 starts = []
 directions = i[0].strip()
+go = {'L':0, 'R':1}
 
 for index, line in enumerate(i[2:]):
     capitals = ''.join(re.findall(r'[A-Z0-9]', line))
-#r'[A-Z]'
     nodes[capitals[0:3]] = (capitals[3:6], capitals[6:9])
     if capitals[2] == 'A':
         starts.append(capitals[0:3])
 
-#place = 'AAA'
-#moves = 1
-go = {'L':0, 'R':1}
-#while place != 'ZZZ':
-#    curr = directions[moves%len(directions)-1]
-#    place = nodes[place][go[curr]]
-#    moves += 1
-
-
-#moves = 1
-#while set(i[2] for i in starts) != set(['Z']):
-
-#    curr = directions[moves%len(directions)-1]
-#    for index, start in enumerate(starts):
-#        starts[index] = nodes[start][go[curr]]
-#    moves += 1
-
-#print(moves-1)
 
 
 loops = []
 for start in starts:
-    tracking = {}
-    curr = start
     fork = 1
     loop = 1
+    tracking = {}
+    curr = start
     leash = -1
     while True:
-        if tracking.get(curr):
-            if leash != -1:
-                loops.append((leash, loop - tracking[curr]))
-                break 
-            else:
-                print('rodeled')
-                loop = tracking[curr]
-        else:
-            tracking[curr] = loop
-
         if curr[2] == 'Z':
             print('tail found')
             leash = fork - 1
-            loop = tracking[curr]
+            print(leash)
+            break
 
 
         curr = nodes[curr][go[directions[fork%len(directions)-1]]]
         fork += 1
-        loop += 1
 
+
+    curr = nodes[curr][go[directions[fork%len(directions)-1]]]
+    fork += 1
+
+    while True:
+        if curr[2] == 'Z':
+            print('loop len found')
+            loop = fork - (leash + 1)
+            print(loop)
+            break
+
+        curr = nodes[curr][go[directions[fork%len(directions)-1]]]
+        fork += 1
+
+    loops.append((leash, loop))
 
 print(loops)
+
+## 10668805667831
 
 for index in range(len(loops)):
     rem, mod = loops[index]
@@ -81,8 +70,8 @@ for index in range(1, len(loops)):
     lcm =  min(lcm, modulon) // math.gcd(lcm, modulon) * max(lcm, modulon)
 
 print(curr)
+
     
 
-## 10668805667831
 
-     
+
