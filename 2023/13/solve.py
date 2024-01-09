efe02bdd85fa22a929603236b2e07ad833b21b49
Carson Magnuson <carsonmagnuson@gmail.com>
2024-01-09 01:26:10 -0500

    messy but done

--- 2023/13/solve.py
+++ 2023/13/solve.py
@@ -14,9 +14,7 @@ def mirrorline(inp: List) -> int:
         if x:
             inp = list(zip(*inp))
 
-        potentials = [
-            index for index in range(len(inp) - 1) if inp[index] == inp[index + 1]
-        ]
+        potentials = [index for index in range(len(inp) - 1) if inp[index] == inp[index + 1]]
 
         for potential in potentials:
             spacer = 3
@@ -35,10 +33,60 @@ def mirrorline(inp: List) -> int:
     print('bad')
     return 0
 
+def diff(l: List, r: List) -> int:
+    """Function to return how many elements are diff between two lists"""
+    return sum(1 for index in range(len(l)) if l[index] != r[index])
+
+def ripple(inp: List, drop: int, tolerance: int) -> bool:
+    """Function to return if mirror image valid within tolerance"""
+    spacer = 3
+    p_index = drop - 1
+    while p_index > -1 and p_index + spacer < len(inp):
+        differ = diff(inp[p_index], inp[p_index + spacer])
+        if differ == 1:
+            if tolerance:
+                tolerance -= 1
+            else:
+                return False
+        if differ > 1:
+            return False
+        spacer += 2
+        p_index -= 1
+
+    return not tolerance    
+
+def smudge(inp: List) -> int:
+    """Function to find smudged mirrorline"""
+    vertical = 0
+    original = mirrorline(inp)
+    if original < 100:
+        vertical = 1
+    avoid = original%100 - 1
+
+    for orientation in range(2):
+        if orientation:
+            inp = list(zip(*inp))
+
+        for index, line in enumerate(inp[:-1]):
+            differ = diff(line, inp[index + 1])
+            if not differ and (index != avoid or orientation != vertical):
+                if ripple(inp, index, 1):
+                    return index + 1 if orientation else (index + 1) * 100
+            if differ == 1:
+                if ripple(inp, index, 0):
+                    return index + 1 if orientation else (index + 1) * 100
+
+    print('bad')
+    return 0
 
 def a(inp: str) -> int:
     """Solution to part A"""
     return sum(mirrorline(mirror) for mirror in parse(inp))
 
+def b(inp: str) -> int:
+    """Solution to part B"""
+    return sum(smudge(mirror) for mirror in parse(inp))
+
 print(a('0'))
+print(b('0'))
 
